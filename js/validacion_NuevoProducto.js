fa03803587b53f2e7654a5197b34ec76028e7a72
hksseba <seb.silvan@duocuc.cl>
2023-04-28 16:32:01 -0400

    Validacion Nuevo producto terminada
    Se crea la validacion para el formulario al momento de ingresar un nuevo producto

---
+++ js/validacion_NuevoProducto.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $("#formProducto").submit(function (e) {
+        e.preventDefault();
+        var nombreProducto = $("#nombreProducto").val();
+        var stock = $("#StockProducto").val();
+        var precio = $("#Precio").val();
+
+        let msjMostrar = "";
+        let enviar = false;
+
+        if(nombreProducto == ""  ){
+            msjMostrar = msjMostrar + "Debe ingresar el nombre del producto ";
+            enviar = true;
+        }
+
+        var letra = nombreProducto.trim().charAt(0);
+        if(!esMayuscula(letra)){
+            msjMostrar += "<br>El nombre debe comenzar con mayúscula";
+            enviar = true;
+        }
+
+        if(stock =="" || stock <1 ){
+            msjMostrar = msjMostrar + "<br>Debe ingresar el stock del producto ";
+            enviar = true;
+            
+        }
+        if(precio == "" || precio <1){
+            msjMostrar = msjMostrar + "<br>Debe ingresar el precio del producto ";
+            enviar = true;
+        }
+
+        if(enviar){
+            $("#warnings").html(msjMostrar);
+        }
+        else{
+            $("#warnings").html("Enviado");
+            $('input[type="text"]').val('');
+            $('input[type="number"]').val('');
+        }
+
+    });
+    function esMayuscula(letra){
+
+        if(letra == letra.toUpperCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+});
